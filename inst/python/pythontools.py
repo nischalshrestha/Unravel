c02882e41f25d0cdcbb9478015ecc5e03f1a8e41
nischalshrestha <bitsorific@gmail.com>
2020-11-18 21:09:29 -0500

    push progress on testing get_last_value

--- inst/python/pythontools.py
+++ inst/python/pythontools.py
@@ -14,31 +14,8 @@ def get_last_value(code):
       return ""
     else:
       last_expr = expressions[-1]
-    # first exec everything to introduce objects to global python environment
-    exec(code)
     # then try eval on the last expression
     if last_expr is not None:
-      out = eval(astunparse.unparse(last_expr).strip())
-      return out
+      return astunparse.unparse(last_expr).strip()
   except Exception as e:
     return None
-
-# code = \
-# """
-# import pandas as pd
-# import numpy as np
-# arrays = [np.array(['bar', 'bar', 'baz', 'baz', 'foo', 'foo', 'qux', 'qux']),
-#         np.array(['one', 'two', 'one', 'two', 'one', 'two', 'one', 'two'])]
-# tuples = list(zip(*arrays))
-# index = pd.MultiIndex.from_tuples(tuples, names=['first', 'second'])
-# df_basic = pd.DataFrame(np.random.randn(8, 4), columns=['a','b','c','d'])
-# 
-# df_basic
-# x = 'hello' # statement should be ignored
-# """
-
-# testing, we should get df_basic back
-# get_last_value(code)
-
-
-
