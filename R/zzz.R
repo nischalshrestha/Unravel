c314c5242ded171851d6ac1b11391c3645f7fd2c
nischalshrestha <bitsorific@gmail.com>
2020-12-02 17:05:20 -0500

    forgot to exclude the unused pythontools module loading

--- R/zzz.R
+++ R/zzz.R
@@ -1,15 +1,15 @@
 # import the `pythontools` module,
-.onLoad <- function(libname, pkgname) {
-  # environment where the `pygradethis` objects will be stored
-  pkg_ns_env <- parent.env(environment())
-  #
-  # python_module <- reticulate::import_from_path(
-  #   module = "pythontools",
-  #   path = system.file("python", package = packageName())
-  # )
-  reticulate::source_python(
-    system.file("python/pythontools.py", package="DataTutor"),
-    envir=pkg_ns_env
-  )
-
-}
+# .onLoad <- function(libname, pkgname) {
+#   # environment where the `pygradethis` objects will be stored
+#   pkg_ns_env <- parent.env(environment())
+#   #
+#   # python_module <- reticulate::import_from_path(
+#   #   module = "pythontools",
+#   #   path = system.file("python", package = packageName())
+#   # )
+#   reticulate::source_python(
+#     system.file("python/pythontools.py", package="DataTutor"),
+#     envir=pkg_ns_env
+#   )
+#
+# }
