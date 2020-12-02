6acd9b442000f1ad82ae6307dca1011ec14bfcec
nischalshrestha <bitsorific@gmail.com>
2020-12-02 16:53:38 -0500

    do not use custom knitr hooks yet

--- R/package.R
+++ R/package.R
@@ -1,8 +1,8 @@
 
 # install knitr hooks when package is attached to search path
-.onAttach <- function(libname, pkgname) {
-  install_knitr_hooks()
-}
+# .onAttach <- function(libname, pkgname) {
+#   install_knitr_hooks()
+# }
 
 # TODO remove knitr hooks when package is detached from search path
 # .onDetach <- function(libpath) {
