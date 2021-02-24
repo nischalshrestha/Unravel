a016b7f70c63543640f43ceaf5406bfe1183e777
nischalshrestha <bitsorific@gmail.com>
2021-02-24 02:05:53 -0500

    started adding R to JS interaction

--- R/datawhats.R
+++ R/datawhats.R
@@ -1,5 +1,14 @@
 library(shiny)
 
+# TODO create a helper function that creates a div for a group item for SortableJS
+# what are the unique identifiers?
+# - Item ID: <id> for group item: data, verb1, verb2, ... verbn.
+#   "verbN" is better for uniquely identifying since you can have multiple of same verb
+# - Item Summary ID: <id>_summary_box, <id>_summary_box_row, <id>_summary_box_col
+# - Item Box Type ID: <change>_square
+# - Item Glyph ID: <id>_glyph
+# - Item Toggle ID: <id>_toggle
+
 datawatsUI <- function(id) {
   # namespace for module
   ns <- shiny::NS(id)
@@ -28,40 +37,34 @@ datawatsUI <- function(id) {
     shiny::fixedPage(
       id = "simpleList", class="list-group",
       # summary box 1
-      div(
-        class = "d-flex list-group-item", `data-id` = "0",
+      div(class = "d-flex list-group-item", id="data", `data-id` = "0",
         # row div
-        div(
-          class = "justify-content-center align-self-baseline",
-          div(
-            class = "d-flex justify-content-center align-self-center",
-            div(
-              class = "row", style = "font-size:0.8em;",
+        div(class = "justify-content-center align-self-baseline",
+          div(class = "d-flex justify-content-center align-self-center",
+            div(class = "row", style = "font-size:0.8em;",
               HTML("&nbsp;")
             )
           ),
-          div(
-            class = "d-flex empty-square justify-content-center",
-            div(
-              class="align-self-center", style="font-size: 0.8em;",
+          div(class = "data_summary_box_row d-flex empty-square justify-content-center",
+            div(class="align-self-center", style="font-size: 0.8em;",
+              # update element
               HTML("1.9M")
             )
           )
         ),
         # column div + square div
-        div(
-          class = "justify-content-center align-self-baseline",
-          div(
-            class = "d-flex justify-content-center align-self-center",
-            div(
-              class = "row", style = "font-size:0.8em;",
+        div(class = "justify-content-center align-self-baseline",
+          div(class = "data_summary_box_col d-flex justify-content-center align-self-center",
+            div(class = "row", style = "font-size:0.8em;",
+              # update element
               HTML("5")
             )
           ),
-          div(class = "d-flex grey-square justify-content-center")
+          # update element (class of square)
+          div(class = "data_summary_box d-flex grey-square justify-content-center")
         ),
         # glyphicon
-        div(class="d-flex justify-content-center align-self-center",
+        div(class="data_glyph d-flex justify-content-center align-self-center",
             span(class="glyphicon glyphicon-move", style="opacity:1;")
         ),
         # codemirror empty div above
@@ -77,8 +80,11 @@ datawatsUI <- function(id) {
             div(class="d-flex justify-content-center align-self-center",
                 div(style="font-size: 0.8em;", HTML("&nbsp;"))
             ),
+            # the value of `checked` is not meaningful, the existence of attribute turns on toggle by default
             shiny::tags$input(
               type = "checkbox",
+              id = "data_toggle",
+              `checked` = TRUE,
               `data-toggle`="toggle",
               `data-size`="xs",
               `data-height`="20", `data-width`="30",
@@ -88,33 +94,24 @@ datawatsUI <- function(id) {
         )
       ),
       # summary box 2... etc
-      div(
-        class = "d-flex list-group-item", id="group_by", `data-id` = "1",
+      div(class = "d-flex list-group-item", id="group_by", `data-id` = "1",
         # row div
-        div(
-          class = "justify-content-center align-self-baseline",
-          div(
-            class = "d-flex justify-content-center align-self-center",
-            div(
-              class = "row", style = "font-size:0.8em;",
+        div(class = "justify-content-center align-self-baseline",
+          div(class = "d-flex justify-content-center align-self-center",
+            div(class = "row", style = "font-size:0.8em;",
               HTML("&nbsp;")
             )
           ),
-          div(
-            class = "d-flex empty-square justify-content-center",
-            div(
-              class="align-self-center", style="font-size: 0.8em;",
+          div(class = "d-flex empty-square justify-content-center",
+            div(class="align-self-center", style="font-size: 0.8em;",
               HTML("1.9M")
             )
           )
         ),
         # column div + square div
-        div(
-          class = "justify-content-center align-self-baseline",
-          div(
-            class = "d-flex justify-content-center align-self-center",
-            div(
-              class = "row", style = "font-size:0.8em;",
+        div(class = "justify-content-center align-self-baseline",
+          div(class = "d-flex justify-content-center align-self-center",
+            div(class = "row", style = "font-size:0.8em;",
               HTML("5")
             )
           ),
@@ -139,6 +136,8 @@ datawatsUI <- function(id) {
             ),
             shiny::tags$input(
               type = "checkbox",
+              id = "group_by_toggle",
+              `checked` = TRUE,
               `data-toggle`="toggle",
               `data-size`="xs",
               `data-height`="20", `data-width`="30",
@@ -187,6 +186,17 @@ datawatsServer <- function(id) {
           shiny::includeScript(here::here("inst/tutorials/datawhats/js/script.js"))
         )
       })
+
+      observeEvent(input$data_toggle, {
+        # this lets us get the boolean value of the toggle from JS side!
+        if (isTRUE(input$data_toggle)) {
+          # TODO
+          session$sendCustomMessage("data_toggle", "un-commenting line!")
+        } else {
+          session$sendCustomMessage("data_toggle", "commenting line!")
+        }
+      })
+
     }
 
   )
--- inst/tutorials/datawhats/js/script.js
+++ inst/tutorials/datawhats/js/script.js
@@ -1,22 +1,12 @@
 
-/*
-var data = CodeMirror.fromTextArea($('#data')[0], {
-  mode: 'r',
-  readOnly: 'nocursor',
-  styleActiveLine: false,
-  lineNumbers: false,
-  firstLineNumber: 1
-});
-data.setSize(null, 50);
-*/
-
-var editors = [];
+var editors = {};
 var sortable = null;
 
 function create_editors() {
   $(".verb").each(function(index, element){
-  	console.log(index, element.id);
-  	if (editors.indexOf(element.id) == -1) {
+    ID = element.id;
+  	console.log(index + " " + ID);
+  	if (!(ID in editors)) {
       let verb_editor = CodeMirror.fromTextArea(element, {
         mode: 'r',
         readOnly: 'nocursor',
@@ -25,13 +15,48 @@ function create_editors() {
         firstLineNumber: 1
       });
       verb_editor.setSize(null, 50);
-      editors.push(element.id);
+      editors[ID] = verb_editor;
   	}
   });
 }
 
 function create_sortable() {
   sortable = Sortable.create(simpleList, { });
+}
+
+function setup_toggles() {
+  // set up the toggle event css listeners
+  $('#data_toggle').change(function() {
+    //console.log('data_toggle: ' + $(this).prop('checked'));
+    editor_wrapper = editors["data"].getWrapperElement();
+  	// TODO programmatically grab the tippy, glyph, summary box, box row and col divs
+    if ($(this).prop('checked')) {
+      editor_wrapper.style.opacity = "1";
+    	/*group_by_tippy.enable();
+      group_by_glyph.style.opacity = "1";
+      group_by_summary_box.style.opacity = "1";
+      group_by_summary_box_row.style.opacity = "1";
+      group_by_summary_box_col.style.opacity = "1";*/
+    } else {
+      editor_wrapper.style.opacity = "0.25";
+    	/*group_by_tippy.disable();
+      group_by_verb_editor_wrapper.style.opacity = "0.25";
+      group_by_glyph.style.opacity = "0.25";
+      group_by_summary_box.style.opacity = "0";
+      group_by_summary_box_row.style.opacity = "0";
+      group_by_summary_box_col.style.opacity = "0";*/
+    }
+  });
+
+  // example of the toggle event listener
+  $('input:checkbox').change(function(){
+    toggle_id = $(this).attr('id')
+    console.log("Event on ID: " + toggle_id);
+  });
+}
+
+function send_toggle(message) {
+  console.log("R received message from JS. " + message);
 }
 
 $(document).ready(function() {
@@ -41,4 +66,19 @@ $(document).ready(function() {
   create_editors();
   // set up the sortablejs
   create_sortable();
+  // set up the toggle listeners
+  setup_toggles();
+  // set up the toggle one way (JS to R) listener
+  // the R side will update the dataframe output, and send info about box, summary box/row/col, and prompt
+  $('#data_toggle').change(function() {
+    Shiny.setInputValue("datawat-data_toggle", $(this).prop('checked'));
+    //console.log('data_toggle: ' + $(this).prop('checked'));
+  })
+  Shiny.addCustomMessageHandler('data_toggle', function(message) {
+    console.log("sending data_toggle a message!");
+    send_toggle(message);
+  });
+
+  // TODO set up the dialog prompt which requires info from R side
+
 });
