3c7ad348f1c5f348a16b606fc5a7ecbd594d986a
nischalshrestha <bitsorific@gmail.com>
2022-02-23 14:56:57 -0500

    change missing value stat to red text

--- R/diagnosis.R
+++ R/diagnosis.R
@@ -31,12 +31,12 @@ get_summary <- function(dat) {
     )
   ))
 
-  # extract missing value count as %
+  # extract missing value count
   pct_missing <- unlist(unname(
     lapply(
       summary,
       function(variable) {
-        round(variable$countMissing$value / nrow(dat), 2)
+        variable$countMissing$value
       }
     )
   ))
@@ -66,7 +66,7 @@ get_diagnosis <- function(dat) {
 
   ### Curated summary
   # list of the variable descriptive stat tables
-  # but exclude variableType, countMissing (these wil be in the outer table)
+  # but exclude variableType, countMissing (these will be in the outer table)
   stables <- lapply(
     summary,
     function(variable) {
@@ -75,6 +75,9 @@ get_diagnosis <- function(dat) {
       features <- features[!features %in% exclude]
       variables <- Filter(function(v) !v %in% exclude, variable)
       tidyr::unnest(dplyr::tibble(
+        # TODO get the human-readable versions of the features
+        # dataReporter does this when reporting issues, just need to
+        # somehow dig that part out or do the renaming ourselves (with key-value list)
         Feature = features,
         Result = lapply(
           features,
@@ -98,12 +101,25 @@ get_diagnosis <- function(dat) {
     bordered = TRUE,
     columns = list(
       missing = colDef(
-        cell = reactablefmtr::data_bars(dat_summary,
-          force_outside = c(0, 100),
-          number_fmt = scales::percent,
-          fill_color = "#f87c6b"
-        )
+        # style text red if there are missing values
+        style = function(value) {
+          if (value > 0) {
+            color <- "#ff4a40"
+            fontWeight <- "bold"
+          } else {
+            color <- "#000"
+            fontWeight <- "normal"
+          }
+          list(color = color, fontWeight = fontWeight)
+        },
+        cell = function(value) {
+          # Render as count (%)
+          paste0(value, " (", round(value / nrow(dat), 2), "%)")
+        }
+        # format = colFormat(percent = TRUE, digits = 1)
       ),
+      # TODO maybe we can style this button better or use some kinda
+      # bootstrap button to make it look less bland
       details = colDef(
         name = "",
         sortable = FALSE,
@@ -119,6 +135,8 @@ get_diagnosis <- function(dat) {
       problems <- unname(lapply(problems, function(c) c$message))
       # then, create list items so they can be displayed as bullet points
       problems <- lapply(unname(problems), function(p) shiny::tags$li(gsub("\\\\", "", p)))
+      # TODO test out a shiny::renderPlot(...) once we integrate the diagnostic table
+      # in Unravel itself
       # display the table of stats and the potential issues side by side
       htmltools::div(
         reactable::reactable(
